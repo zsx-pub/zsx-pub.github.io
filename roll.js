c49b1e83720ce8a43e54d78f25e5d7f3ec3b5950
zsx <7544737+zsx_pp@user.noreply.gitee.com>
2020-06-22 20:38:37 +0800

    Site updated: 2020-06-22 20:38:30

--- roll.js
+++ roll.js
@@ -1,7 +1,10 @@
 function showArticle(){
 	$(".article").each(function(){
 	if(!!yiliaConfig.isHome) {
-		if( $(this).offset().top <= $(window).scrollTop()+$(window).height() && !($(this).hasClass('show')) ) {
+    if(!!browser.versions.mobile){
+        
+    } else {
+        	if( $(this).offset().top <= $(window).scrollTop()+$(window).height() && !($(this).hasClass('show')) ) {
 			$(this).css("visibility","visible");
 			$(this).addClass("is-hiddened"); 
 			$(this).css("transform-origin","right");
@@ -14,9 +17,32 @@ function showArticle(){
 
 			}
 		}
+    }
+
 	}
 	})
 }
+
+
+var browser = {
+        versions: function() {
+        var u = window.navigator.userAgent;
+        return {
+            trident: u.indexOf('Trident') > -1, //IE�ں�
+            presto: u.indexOf('Presto') > -1, //opera�ں�
+            webKit: u.indexOf('AppleWebKit') > -1, //ƻ�����ȸ��ں�
+            gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //����ں�
+            mobile: !!u.match(/AppleWebKit.*Mobile.*/), //�Ƿ�Ϊ�ƶ��ն�
+            ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios�ն�
+            android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android�ն˻���uc�����
+            iPhone: u.indexOf('iPhone') > -1 || u.indexOf('Mac') > -1, //�Ƿ�ΪiPhone���߰�׿QQ�����
+            iPad: u.indexOf('iPad') > -1, //�Ƿ�ΪiPad
+            webApp: u.indexOf('Safari') == -1 ,//�Ƿ�ΪwebӦ�ó���û��ͷ����ײ�
+            weixin: u.indexOf('MicroMessenger') == -1 //�Ƿ�Ϊ΢�������
+            };
+        }()
+    }
+
 showArticle();
 var scrollFunc = function (e) { 
 
